2541b5d18b4533d575c5c983b94cb7064fdc885c
Thorto <theotimdelan@hotmail.fr>
2015-12-03 09:41:29 +0100

    CA BUG

--- TerrainJeux.java
+++ TerrainJeux.java
@@ -1,20 +1,49 @@
 import java.io.*;
+import java.lang.*;
 import java.util.*;
 
-public class TerrainJeux
-{
-    private int longueur
-    private int largeur
-    
-    public TerrainJeux(int longueur, int largeur)
-    {
-        this.longueur = longueur;
-        this.largeur = largeur;
-    }
-    
-    public Terrainjeux()
-    {
-        this.longueur=6;
-        this.largeur=7;
-    }
+public class TerrainDeJeu{
+	private int colonne;
+	private int ligne;
+	private String terrain[][];
+	private int cptcol;
+	private int cptligne;
+
+	TerrainDeJeu(){
+		this.colonne=7;
+		this.ligne=6;
+		CreationTerrain();
+	}
+
+	TerrainDeJeu(int col, int ligne){
+		this.colonne=col;
+		this.ligne=ligne;
+		CreationTerrain();
+	}
+
+	public void CreationTerrain(){
+		String terrain[][]=new String[this.ligne][this.colonne];
+		while(cptcol<this.colonne){
+			while(cptligne<this.ligne){
+				terrain[cptligne][cptcol]="-";
+				cptligne++;
+			}
+			cptligne=0;
+			cptcol++;
+		}
+		cptcol=0;
+	}
+
+	public void AfficherTerrain(){
+		while(cptcol<this.colonne){
+			while(cptligne<this.ligne){
+				System.out.println(terrain[1][1]);
+				cptligne++;
+			}
+			cptligne=0;
+			cptcol++;
+		}
+		cptcol=0;
+	}
 }
+
