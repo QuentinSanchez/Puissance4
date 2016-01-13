0fc7f5dc38e8ffe5fd0e7d69570e2f7cfa759121
neexs <guillaume.gebavi@yahoo.com>
2016-01-13 18:19:11 +0200

    Mise à jour de la classe Jeton
    Ajout des constructeurs

--- Jeton.java
+++ Jeton.java
@@ -1,10 +1,16 @@
-public class Jeton{
+public class Jeton {
+    private int colonne;
+    private int ligne;
+    public Jeton(){
+        
+    }
+
+    public Jeton(int colonne, int ligne){
+        this.colonne=colonne;
+        this.ligne=ligne;
+    }
 
 
 
-
-
-
-
-
+    
 }
