c01aa72aa4dec91f1d2083943aa1a8821de9aaf2
Thorto <theotimdelan@hotmail.fr>
2016-01-13 17:06:41 +0100

    Fini

--- TerrainJeux.java
+++ TerrainJeux.java
@@ -1,49 +1,55 @@
-import java.io.*;
-import java.lang.*;
-import java.util.*;
+package puissance4;
 
-public class TerrainDeJeu{
-	private int colonne;
-	private int ligne;
-	private String terrain[][];
-	private int cptcol;
-	private int cptligne;
-
-	TerrainDeJeu(){
-		this.colonne=7;
-		this.ligne=6;
-		CreationTerrain();
-	}
-
-	TerrainDeJeu(int col, int ligne){
-		this.colonne=col;
-		this.ligne=ligne;
-		CreationTerrain();
-	}
-
-	public void CreationTerrain(){
-		String terrain[][]=new String[this.ligne][this.colonne];
-		while(cptcol<this.colonne){
-			while(cptligne<this.ligne){
-				terrain[cptligne][cptcol]="-";
-				cptligne++;
-			}
-			cptligne=0;
-			cptcol++;
-		}
-		cptcol=0;
-	}
-
-	public void AfficherTerrain(){
-		while(cptcol<this.colonne){
-			while(cptligne<this.ligne){
-				System.out.println(terrain[1][1]);
-				cptligne++;
-			}
-			cptligne=0;
-			cptcol++;
-		}
-		cptcol=0;
-	}
-}
-
+/**
+ *
+ * @author Pierre
+ */
+public class TerrainDeJeu {
+ 	private int colonne;
+ 	private int ligne;
+ 	private String terrain[][];
+ 	private int cptcol;
+ 	private int cptligne;
+ 
+ 	TerrainDeJeu(){
+ 		this.colonne=7;
+ 		this.ligne=6;
+ 		CreationTerrain();
+ 	}
+ 
+ 	TerrainDeJeu(int col, int ligne){
+ 		this.colonne=col;
+ 		this.ligne=ligne;
+ 		CreationTerrain();
+ 	}
+ 
+ 	public void CreationTerrain(){
+            cptcol=0;
+            cptligne=0;
+            terrain=new String[this.ligne][this.colonne];
+            while(cptcol<this.colonne){
+                while(cptligne<this.ligne){
+                    terrain[cptligne][cptcol]=" - ";
+                    cptligne++;
+                }
+                cptligne=0;
+                cptcol++;
+            }
+            cptcol=0;
+ 	}
+ 
+ 	public void AfficherTerrain(){
+            cptligne=0;
+            cptcol=0;
+ 		while(cptcol<this.colonne){
+ 			while(cptligne<this.ligne){
+ 				System.out.print(terrain[cptligne][cptcol]);
+ 				cptligne++;
+ 			}
+                        System.out.println();
+ 			cptligne=0;
+ 			cptcol++;
+ 		}
+ 		cptcol=0;
+ 	}
+  }
