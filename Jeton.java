6ffaf69f0d1ee2d8e105353522de5aa92f85cdc2
neexs <guillaume.gebavi@yahoo.com>
2016-01-13 18:45:48 +0200

    + insérer, + constructeur

--- Jeton.java
+++ Jeton.java
@@ -1,14 +1,30 @@
-public class Jeton {
+
+package jeton;
+
+public class Jeton{
     private int colonne;
     private int ligne;
+    private String motif;
+    private TerrainDeJeu terrain;
+    
     public Jeton(){
         
     }
 
-    public Jeton(int colonne, int ligne){
+    //création du jeton avec comme paramètre la couleur (à compléter)
+    public Jeton(String motif){ 
+        this.motif=motif;
+       }
+    
+    //méthode qui permet d'insérer le jeton dans la GRILLE (TerrainDeJeu) à la ligne-colonne en paramètre
+    public void insererJeton(TerrainDeJeu terrain, int colonne, int ligne){
         this.colonne=colonne;
         this.ligne=ligne;
+        terrain = new TerrainDeJeu(colonne,ligne);
+        
     }
+    
+    
 
 
 
